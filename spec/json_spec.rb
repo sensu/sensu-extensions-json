46d32873245651490839dfb41e09310925a9ad47
Sean Porter <portertech@gmail.com>
2016-07-27 15:22:55 -0700

    minor spec description edit

--- spec/json_spec.rb
+++ spec/json_spec.rb
@@ -8,7 +8,7 @@ describe "Sensu::Extension::Json" do
     @extension = Sensu::Extension::Json.new
   end
 
-  it "can run" do
+  it "can run, returning json formatted event data" do
     event = event_template
     @extension.safe_run(event) do |output, status|
       expect(output).to eq(Sensu::JSON.dump(event))
